0dc6c11ef974f7659096a24fabd56d9159d78dc1
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-04-23 23:37:44 +0700

    setup project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  white: "#F4F4F4",
+  orange: "#FF7314",
+  ligthblack: "#393534",
+  black: "#22211F",
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClientRef = React.useRef();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
