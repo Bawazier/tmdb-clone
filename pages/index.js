1e26156818a1e621ee7d326e99dfe005a45eb2f5
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-04-29 08:49:45 +0700

    initial person page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 // import Head from "next/head";
-import { VStack, Container } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/en-US/movies/popular",
+      permanent: false,
+    },
+  };
+}
 
 export default function Home() {
   return (
-    <>
-      <VStack align="left" bg="black" spacing="40px">
-        <Container>
-        </Container>
-      </VStack>
-    </>
+    <Container>
+    </Container>
   );
 }
