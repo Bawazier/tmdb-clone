e5cc63da7b893c5a27601c938853e074a1fc7c2a
ubuntu <muhammadba.wazieer@gmail.com>
2023-02-28 00:01:52 +0700

    updated custom theme chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import {
   Hydrate,
