1d43913bc871aa556fcd9ba464def7cd5ea8c253
ubuntu <muhammadba.wazieer@gmail.com>
2023-02-28 13:14:18 +0700

    try test todo

---
+++ __tests__/home.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "@/pages";
+
+describe("Home Pages", () => {
+  it.todo("should renders Header");
+  it.todo("should renders Footer");
+});
